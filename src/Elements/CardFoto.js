b53a829de6673b3e972ba5c4ce9b448ba6c899a4
Igor Guilherme <igorloredo12@gmail.com>
2020-08-20 16:30:43 -0300

    melhoris

--- src/Elements/CardFoto.js
+++ src/Elements/CardFoto.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Jpson from '../Json/FaunaCard.json'
+import image from '../Imagens/Abacateiro.png'
 
 const Card = (props) => {
+ 
+ console.log(props.Imagem);
+
   return (   
-    <div class="col-md-4">
-        <div class="card mb-4 shadow-sm">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-            <p class="card-text">
-                
+    <div className="col-md-4">
+        <div className="card mb-4 shadow-sm">        
+          <img className="bd-placeholder-img card-img-top" src={image} alt={props.Descricao}/>
+            <div className="card-body">
+            <p className="card-text">
+              {props.Text}
                 </p>          
             
             </div>
--- src/pages/Fauna.js
+++ src/pages/Fauna.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import Navbar from '../Elements/Navbar'
 import Rodape from '../Elements/Rodape'
 import Card from '../Elements/CardFoto'
+import Jpson from '../Json/FaunaCard.json'
+
 
 export default class Fauna extends Component {
     render() {
+       const card = {
+            "image": '.'
+
+       }
         return (
             <div>
                 <Navbar/>
                 <main>
-                    <h3> Fauna:</h3>
+                    <h3 className = "text-center "> Fauna:</h3>
 
                     <div className="album py-5 bg-light">
                         <div className = "container">
-                            <div className = "row">                                
+                            <div className = "row">     
+                            <Card {...card}/>
+                            
                              </div>                              
                         </div>
                     </div>
